187b581253d3f5a12008f3400f947d8486fb34f1
Taerogrammer <hyung54321@naver.com>
2025-06-03 21:05:31 +0900

    refactor: UIImage+Extension 분리

--- PickMeUp/Features/ProfileEdit/ProfileEditEffect.swift
+++ PickMeUp/Features/ProfileEdit/ProfileEditEffect.swift
@@ -84,23 +84,3 @@ struct ProfileEditEffect {
         )
     }
 }
-
-extension UIImage {
-    func inferredFormat() -> ImageFormat {
-        if let data = self.pngData(), data.starts(with: [0x89, 0x50, 0x4E, 0x47]) {
-            return .png
-        } else {
-            return .jpeg
-        }
-    }
-
-    var isPNG: Bool {
-        guard let data = self.pngData() else { return false }
-        return data.starts(with: [0x89, 0x50, 0x4E, 0x47])
-    }
-
-    var isJPEG: Bool {
-        guard let data = self.jpegData(compressionQuality: 1.0) else { return false }
-        return data.starts(with: [0xFF, 0xD8])
-    }
-}
