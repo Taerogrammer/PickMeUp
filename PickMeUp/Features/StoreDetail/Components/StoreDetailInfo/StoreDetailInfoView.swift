23b90f87c7e05529b50595c0caae6ac8077223b0
Taerogrammer <hyung54321@naver.com>
2025-06-05 23:58:16 +0900

    feat: StoreDetailInfoView 디자인

--- PickMeUp/Features/StoreDetail/Components/StoreDetailInfo/StoreDetailInfoView.swift
+++ PickMeUp/Features/StoreDetail/Components/StoreDetailInfo/StoreDetailInfoView.swift
@@ -11,19 +11,60 @@ struct StoreDetailInfoView: View {
     let entity: StoreDetailInfoEntity
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 6) {
-            Label(entity.address, systemImage: "mappin.and.ellipse")
-            Label(entity.open, systemImage: "clock")
-            Label(entity.parkingGuide, systemImage: "parkingsign.circle")
+        VStack(alignment: .leading, spacing: 12) {
+            HStack(alignment: .top, spacing: 8) {
+                Text("가게주소")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                Image(systemName: "location.fill")
+                    .foregroundColor(.deepSprout)
+                Text(entity.address)
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+
+            HStack(alignment: .top, spacing: 8) {
+                Text("영업시간")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                Image(systemName: "clock")
+                    .foregroundColor(.deepSprout)
+                Text("매일 \(entity.open) ~ \(entity.close)")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+
+            HStack(alignment: .top, spacing: 8) {
+                Text("주차여부")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                Image(systemName: "parkingsign.circle.fill")
+                    .foregroundColor(.deepSprout)
+                Text(entity.parkingGuide)
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
         }
-        .font(.footnote)
         .padding()
-        .background(Color.gray.opacity(0.1))
-        .cornerRadius(8)
+        .background(
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color.gray0) // 배경색
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.gray.opacity(0.2), lineWidth: 1) // 테두리
+                )
+        )
         .padding(.horizontal)
     }
 }
 
-//#Preview {
-//    StoreDetailInfoView()
-//}
+
+#Preview {
+    let mockEntity = StoreDetailInfoEntity(
+        address: "서울시 강남구 도산대로 123",
+        open: "10:00", close: "22:00",
+        parkingGuide: "건물 지하 주차장 이용 가능"
+    )
+
+    StoreDetailInfoView(entity: mockEntity)
+}
