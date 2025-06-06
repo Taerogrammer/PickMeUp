87a6397e271c85e94999475a94d79cbb09ff4d99
Taerogrammer <hyung54321@naver.com>
2025-06-06 14:38:25 +0900

    refactor: TabItem 리팩토링

--- PickMeUp/Features/StoreDetail/Components/StoreMenuCategoryTab/StoreMenuCategoryTabView.swift
+++ PickMeUp/Features/StoreDetail/Components/StoreMenuCategoryTab/StoreMenuCategoryTabView.swift
@@ -13,25 +13,62 @@ struct StoreMenuCategoryTabView: View {
 
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 12) {
-                ForEach(entity.categories, id: \.self) { category in
-                    Button(action: {
-                        onSelect(category)
-                    }) {
-                        Text(category)
-                            .padding(.vertical, 6)
-                            .padding(.horizontal, 12)
-                            .background(category == entity.selectedCategory ? Color.orange : Color.gray.opacity(0.2))
-                            .foregroundColor(category == entity.selectedCategory ? .white : .black)
-                            .cornerRadius(16)
-                    }
-                }
+            HStack(spacing: 4) {
+
+                CategoryTabItem(
+                    title: "전체",
+                    isSelected: true,
+                    icon: nil
+                )
+
+                CategoryTabItem(title: "인기메뉴", isSelected: false, icon: nil)
+                CategoryTabItem(title: "수제도넛", isSelected: false, icon: nil)
+                CategoryTabItem(title: "수제 젤라또", isSelected: false, icon: nil)
             }
-//            .padding(.horizontal)
+            .padding(.horizontal)
         }
     }
 }
 
-//#Preview {
-//    StoreMenuCategoryTabView()
-//}
+struct CategoryTabItem: View {
+    let title: String
+    let isSelected: Bool
+    let icon: Image?
+
+    var body: some View {
+        HStack(spacing: 4) {
+            if let icon = icon {
+                icon
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 14, height: 14)
+            }
+
+            Text(title)
+                .fontWeight(isSelected ? .bold : .regular)
+                .font(.system(size: 14))
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal, 14)
+        .background(isSelected ? Color.deepSprout : Color.gray0)
+        .foregroundColor(isSelected ? Color.gray0 : Color.gray60)
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(isSelected ? Color.deepSprout : Color.gray30, lineWidth: 1)
+        )
+        .cornerRadius(16)
+    }
+}
+
+#Preview {
+    StoreMenuCategoryTabView(
+        entity: StoreMenuCategoryTabEntity(
+            selectedCategory: "커피",
+            categories: ["전체", "커피", "디저트"]
+        ),
+        onSelect: { selected in
+            print("선택된 카테고리: \(selected)")
+        }
+    )
+    .padding()
+}
